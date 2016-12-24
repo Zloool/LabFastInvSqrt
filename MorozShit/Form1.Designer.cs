ea93859395e9939299045acf2c645209428c779e
Zloool <zhavoronkov.p@gmail.com>
2016-12-24 21:23:30 +0200

    Initial commit

---
+++ MorozShit/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MorozShit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(35, 197);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(0, 13);
+            this.lb1.TabIndex = 0;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(12, 12);
+            this.tb3.Name = "tb3";
+            this.tb3.ReadOnly = true;
+            this.tb3.Size = new System.Drawing.Size(257, 20);
+            this.tb3.TabIndex = 3;
+            // 
+            // tb4
+            // 
+            this.tb4.Location = new System.Drawing.Point(12, 38);
+            this.tb4.Name = "tb4";
+            this.tb4.ReadOnly = true;
+            this.tb4.Size = new System.Drawing.Size(100, 20);
+            this.tb4.TabIndex = 4;
+            this.tb4.Text = "0";
+            // 
+            // tb5
+            // 
+            this.tb5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb5.Location = new System.Drawing.Point(488, 12);
+            this.tb5.Multiline = true;
+            this.tb5.Name = "tb5";
+            this.tb5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb5.Size = new System.Drawing.Size(540, 379);
+            this.tb5.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 327);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 403);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tb5);
+            this.Controls.Add(this.tb4);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.tb1_TextChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MorozShit/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MorozShit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static unsafe Double InvSqrt(float x)
+        {
+            float HalfNumber = x * (float)0.5;
+            int i = *(int*)&x;
+            i = 0x5f3759df - (i >> 1);
+            x = *(float*)&i;
+            x = x * ((float)1.5 - HalfNumber * x * x);
+            x = x * ((float)1.5 - HalfNumber * x * x);
+            return x;
+        }
+        private Double FindMin(Double start, Double end, Double step, Int32 Accuracy)
+        {
+
+            //tb3.Text = start.ToString();
+            tb4.Text = (Convert.ToInt32(tb4.Text) + 1).ToString();
+            Double perdole = ((double)1.00000000 / Math.Pow(10, Accuracy));
+            if (step < perdole)
+                return start;
+            Double min = 10000, tmp, minid = 0;
+            for (Double i = start; i < end; i += step)
+            {
+                tmp = InvSqrt((float)i);
+                if (tmp < min)
+                {
+                    min = tmp; minid = i;
+                }
+            }
+            tb5.Text = tb5.Text + minid + " : " + min + "\r\n";
+            return FindMin(minid - step, minid + step, step / 10, Accuracy);
+        }
+        private Double FindMinDiff(Double start, Double end, Double step, Int32 Accuracy)
+        {
+
+            //tb3.Text = start.ToString();
+            try
+            {
+                tb4.Text = (Convert.ToInt32(tb4.Text) + 1).ToString();
+            }
+            catch (Exception ex) { }
+            Double perdole = ((double)1.00000000 / Math.Pow(10, Accuracy));
+            if (step < perdole)
+                return start;
+            Double min = 10000, tmp, minid = 0;
+            for (Double i = start; i < end; i += step)
+            {
+                tmp = (1 / Math.Sqrt(i)) - InvSqrt((float)i);
+                if (tmp < min)
+                {
+                    min = tmp; minid = i;
+                }
+            }
+            try
+            {
+                tb5.Text = tb5.Text + minid + " : " + min + " " + (1 / Math.Sqrt(minid)) + " " + InvSqrt((float)minid) + "\r\n";
+            } 
+            catch (Exception ex) { }
+            Console.Beep(300, 100);
+            return FindMinDiff(minid - step, minid + step, step / 10, Accuracy);
+        }
+
+        void FindAll(Double start, Double end, Double step)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            PointF[] ptf = new PointF[(int)((end - start) / step)];
+
+            // число волн
+
+
+            // Очистим PictureBox
+            g.Clear(pictureBox1.BackColor);
+            int dick = 0, scale = 150;
+            for (Double i = start; i < end; i += step)
+            {
+                ptf[dick].X = pictureBox1.Width - (float)i * scale;
+                ptf[dick].Y = (float)InvSqrt((float)i) * scale;
+                //ptf[dick].Y = (float)(1 / Math.Sqrt(i)) * scale;
+                dick++;
+                try
+                {
+                    tb5.Text = tb5.Text + i + " : " + InvSqrt((float)i) + "\r\n";
+                }
+                catch (Exception ex)
+                { }
+            }
+            g.DrawLines(Pens.Red, ptf);
+            g.RotateTransform(30.0F);
+            //g.Dispose();
+        }
+        void FindAll2(Double start, Double end, Double step)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            PointF[] ptf = new PointF[(int)((end - start) / step)];
+
+            // число волн
+
+
+            // Очистим PictureBox
+            g.Clear(pictureBox1.BackColor);
+            int dick = 0, scale = 150;
+            for (Double i = start; i < end; i += step)
+            {
+                ptf[dick].X = pictureBox1.Width - (float)i * scale;
+                //ptf[dick].Y = (float)InvSqrt((float)i) * scale;
+                ptf[dick].Y = (float)(1 / Math.Sqrt(i)) * scale;
+                dick++;
+                try
+                {
+                    tb5.Text = tb5.Text + i + " : " + InvSqrt((float)i) + "\r\n";
+                }
+                catch (Exception ex)
+                { }
+            }
+            g.DrawLines(Pens.Green, ptf);
+            g.RotateTransform(30.0F);
+            //g.Dispose();
+        }
+        void FindDiff(Double start, Double end, Double step)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            PointF[] ptf = new PointF[(int)((end - start) / step)];
+
+            // число волн
+
+
+            // Очистим PictureBox
+            g.Clear(pictureBox1.BackColor);
+            int dick = 0, scale = 150;
+            for (Double i = start; i < end; i += step)
+            {
+                ptf[dick].X = /*pictureBox1.Width - */(float)i * scale;
+                //ptf[dick].Y = (float)InvSqrt((float)i) * scale;
+                ptf[dick].Y = ((float)InvSqrt((float)i) - (float)(1 / Math.Sqrt(i))) * scale*10000000;
+                dick++;
+                try
+                {
+                    tb5.Text = tb5.Text + i + " : " + InvSqrt((float)i) + "\r\n";
+                }
+                catch (Exception ex)
+                { }
+            }
+            g.DrawLines(Pens.Pink, ptf);
+            //g.RotateTransform(30.0F);
+            //g.Dispose();
+        }
+        private void Calculate()
+        {
+            this.Text = "Working on it...";
+            //Double z = FindMin(0.5, 2, 0.0001, 20);
+            //FindAll(0.5, 2, 0.01);
+            //FindAll2(0.5, 2, 0.01);
+            FindDiff(0.5, 2, 0.01);
+            //FindMinDiff(0.5,  2, 0.0001, 16);//Больше 16 не работает!
+            //FindMinDiff(1.3, 2, 0.0001, 20);
+            //FindMinDiff(0.9, 2, 0.0001, 20);
+            this.Text = "Done!";
+        }
+
+        private void tb1_TextChanged(object sender, EventArgs e)
+        {
+
+            //FindMinDiff(0.5, 2, 0.0001, 26);
+            //0.5 - 2.0 1*2-23 відносна похибка, визначити максимальну відносну 1 - 4
+            Task.Factory.StartNew(Calculate);
+        }
+
+    }
+}
